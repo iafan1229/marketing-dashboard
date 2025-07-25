cb5f863c90f827bdf2c29031dab24f9cb73e8d53
iafan1229 <home0@naver.com>
2025-07-25 09:08:08 +0900

    Update page.tsx

--- src/app/dashboard/list/page.tsx
+++ src/app/dashboard/list/page.tsx
@@ -1,4 +1,4 @@
-import { DashboardListContainer } from "@/components/features/dashboardList/dashboardListContainer";
+import { DashboardListContainer } from "@/components/features/dashboardList/DashboardListContainer";
 
 export default function DashboardListPage() {
   return <DashboardListContainer />;
